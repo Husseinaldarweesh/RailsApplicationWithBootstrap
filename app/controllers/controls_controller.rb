9b15fe57ca0b1f424a4b48e425db2ff0f336a6e0
Hussein.Aldarweesh <husseinaldarweesh@gmail.com>
2019-07-18 01:27:45 +0300

    commit

---
+++ app/controllers/controls_controller.rb
@@ -0,0 +1,22 @@
+class ControlsController < ApplicationController
+	def login
+		if request.post?
+			user= User.find_by(:email=>params[:email], :password=>params[:password])
+			if !user.nil?
+				session[:user] = user.id
+				redirect_to root_url, notic: "Thank you for signing up"
+
+			else
+				respond_to do |format|
+					format.html {render :new}
+					format.json {render json: @user.errors, status: :unprocessable_entity }
+				end
+			end
+
+		end
+	end
+	def logout
+		session[:user]=nil
+		redirect_to root_url, notic: "Thank you for signing up"
+	end
+end
---
+++ app/helpers/our_project_helper.rb
@@ -0,0 +1,2 @@
+module OurProjectHelper
+end
